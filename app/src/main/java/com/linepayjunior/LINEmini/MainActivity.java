eb666f2d2f7cfc0fcbfe8c89c7e0dd5a6950498f
swon96 <ohsing0906@gmail.com>
2019-11-12 13:45:34 +0900

    191111 Enter 추가

--- app/src/main/java/com/linepayjunior/LINEmini/MainActivity.java
+++ app/src/main/java/com/linepayjunior/LINEmini/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
+
+
 import android.os.Bundle;
 import android.view.MenuItem;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
