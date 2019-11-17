8b841c41b33194f4e318002022a57ea533328646
swon96 <ohsing0906@gmail.com>
2019-11-18 02:16:53 +0900

    191118 화면 추가

--- app/src/main/java/com/linepayjunior/LINEmini/MainActivity.java
+++ app/src/main/java/com/linepayjunior/LINEmini/MainActivity.java
@@ -3,7 +3,7 @@ package com.linepayjunior.LINEmini;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.appcompat.text.*;
 
 
 
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.qrcode);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
